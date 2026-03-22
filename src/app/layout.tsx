32d5cd3abd8341ad4770b2a492e17139260ec37c
somi bak <somi.bak@gmail.com>
2026-03-22 19:15:40 +0900

    4. 뼈대 만들기

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,24 @@ export default function RootLayout({
 }>) {
   return (
     <html>
-      <body>{children}</body>
+      <body>
+        <h1><a href="/">WEB</a></h1>
+
+        <ol>
+          <li><a href="/read/1">html</a></li>
+          <li><a href="/read/2">css</a></li>
+    
+        </ol>
+
+        {children}
+
+        <ul>
+          <li><a href="/create">Create</a></li>
+          <li><a href="/update/1">Update</a></li>
+          <li><input type="button" value="delete" /></li>        
+        </ul>
+        
+        </body>
     </html>
   );
 }
