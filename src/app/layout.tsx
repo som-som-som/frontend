d742431f894d7591eedc70c6ea4919e5235e86ba
somi bak <somi.bak@gmail.com>
2026-03-22 19:11:35 +0900

    2. 샘플앱 세탁

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { Geist } from "next/font/google";
-import { cn } from "@/lib/utils";
-
-const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
   title: "App",
@@ -16,10 +12,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko" className={cn("font-sans", geist.variable)}>
+    <html>
       <body>{children}</body>
     </html>
   );
 }
-
-// 첫번째 커밋
